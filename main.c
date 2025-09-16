30b4f17c4445b42da530d37ba013fc6a83f6580b
Michael Wilson <mlwilson04@icloud.com>
2025-09-16 16:29:01 +1000

    convert to DDA raycasting algorithm

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 #include <raylib.h>
 #include <raymath.h>
 
-#define RAY_LEN 2.0f
-#define STEP_LEN 0.1f
+#define MAX_RAY_LEN 2.0f
 #define FOV 90
 #define ROT_SPEED 120
 #define MOVE_SPEED 1.0f
@@ -53,7 +52,7 @@ int main (void) {
     bool sprinting = IsKeyDown(KEY_W) && IsKeyDown(KEY_LEFT_SHIFT);
     vel = Vector2Rotate(Vector2Scale(Vector2Normalize(vel), MOVE_SPEED * (1.0f + sprinting) * delta), rot);
 
-    // Apply velcoity
+    // Apply velocity
     Vector2 new_pos = Vector2Add(pos, vel);
     new_pos.x = wrap_geq(new_pos.x, 0, map.width);
     new_pos.y = wrap_geq(new_pos.y, 0, map.height);
@@ -67,25 +66,66 @@ int main (void) {
     
     for (int x = 0; x < RENDER_X; x++) {
       float ray_angle = ((float)x / RENDER_X - 0.5) * FOV * DEG2RAD + rot;
-      Vector2 ray_step = Vector2Rotate((Vector2){STEP_LEN, 0}, ray_angle);
-      Vector2 ray_pos = pos;
+      Vector2 ray_dir = Vector2Rotate((Vector2){1, 0}, ray_angle);
+      bool x_first = fabsf(ray_dir.x) > fabsf(ray_dir.y);
+      float slope = fabsf(ray_dir.x / ray_dir.y);
+      int step_x;
+      int step_y;
+      int grid_x = (int)pos.x;
+      int grid_y = (int)pos.y;
+      float hyp_dist_x = fabsf(1 / ray_dir.x);
+      float hyp_dist_y = fabsf(1 / ray_dir.y);
+      float dist_x;
+      float dist_y;
+      int hit_side = 0;
+      Color col_color = BLACK;
 
-      for (float f = 0.0f; f < RAY_LEN; f += STEP_LEN) {
-        ray_pos = Vector2Add(ray_pos, ray_step);
-
-        float cell_x = wrap_geq(ray_pos.x, 0, map.width);
-        float cell_y = wrap_geq(ray_pos.y, 0, map.height);
-        Color col_color = GetImageColor(map, floorf(cell_x), floorf(cell_y));
-
-        if (!ColorIsEqual(col_color, BLACK))  {
-          int col_height = RENDER_Y * (1 - f / RAY_LEN);
-          int col_start = (RENDER_Y - col_height) * 0.5;
-          col_color = ColorLerp(col_color, BLACK, f / RAY_LEN);
-
-          DrawLine(x, col_start, x, col_start + col_height, col_color);
-          break;
-        }
+      if (ray_dir.x >= 0 ) {
+        step_x = 1;
+        dist_x = (grid_x + 1.0f - pos.x) * hyp_dist_x;
       }
+      else {
+        step_x = -1;
+        dist_x = (pos.x - grid_x) * hyp_dist_x;
+      }
+      if (ray_dir.y >= 0 ) {
+        step_y = 1;
+        dist_y = (grid_y + 1.0f - pos.y) * hyp_dist_y;
+      }
+      else {
+        step_y = -1;
+        dist_y = (pos.y - grid_y) * hyp_dist_y;
+      }
+  
+      while (true) {
+        if (dist_x < dist_y) {
+          dist_x += hyp_dist_x;
+          grid_x += step_x;
+          hit_side = 0;
+        }
+        else {
+          dist_y += hyp_dist_y;
+          grid_y += step_y;
+          hit_side = 1;
+        }
+
+        col_color = GetImageColor(map, wrap_geq(grid_x, 0, map.width), wrap_geq(grid_y, 0, map.height));
+        if (!ColorIsEqual(col_color, BLACK) || fminf(dist_x, dist_y) >= MAX_RAY_LEN) break; // ERROR WITH LENGTH CHECK
+      }
+      
+      float ray_len;
+      if (hit_side == 0) {
+        ray_len = dist_x - hyp_dist_x;
+      }
+      else {
+        ray_len = dist_y - hyp_dist_y;
+      }
+
+      int col_height = RENDER_Y * (1 - ray_len / MAX_RAY_LEN);
+      int col_start = (RENDER_Y - col_height) * 0.5;
+      col_color = ColorLerp(col_color, BLACK, ray_len / MAX_RAY_LEN);
+
+      DrawLine(x, col_start, x, col_start + col_height, col_color);
     }
 
     EndTextureMode();
